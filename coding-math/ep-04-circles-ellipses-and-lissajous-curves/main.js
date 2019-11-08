8195033cd88548edd794063e718fff90de8f95f2
Victor Lawrence <victor.lawrence@uga.edu>
2019-11-08 13:46:18 -0500

    Ah, look at this beautifully animated circle rotating in a circle!

--- coding-math/ep-04-circles-ellipses-and-lissajous-curves/main.js
+++ coding-math/ep-04-circles-ellipses-and-lissajous-curves/main.js
@@ -9,8 +9,31 @@ const context = canvas.getContext('2d')
 
 let shouldContinue = true
 
+let angle = 0
+
 const render = ({ timestamp, resized, context, canvas }) => {
   clearCanvas({ context })
+  const { width, height } = canvas
+
+  const centerX = width * 0.5
+  const centerY = height * 0.5
+
+  const radius = 200
+  const speed = 0.1
+
+  const x = centerX + Math.cos(angle) * radius
+  const y = centerY + Math.sin(angle) * radius
+
+  context.beginPath()
+  context.moveTo(centerX, centerY)
+  context.lineTo(x, y)
+  context.stroke()
+
+  context.beginPath()
+  context.arc(x, y, 10, 0, Math.PI * 2, false)
+  context.fill()
+
+  angle += speed
 }
 
 const heartbeat = timestamp => {
--- coding-math/ep-04-circles-ellipses-and-lissajous-curves/readme.md
+++ coding-math/ep-04-circles-ellipses-and-lissajous-curves/readme.md
@@ -2,3 +2,19 @@ Episode: https://www.youtube.com/watch?v=SoYnZHBP-6M
 
 # Circles, Ellipses and Lissajous Curves
 
+Note: `Lissajous` is pronounced `liss-ah-jew`
+
+---
+
+Now we are going to try do animate objects in a circle.
+
+To do this, think of the `hypotenuse` as the `radius` of the circle.
+
+```js
+
+const angle = // some value in radians
+const radius = // some value
+const x = radius * Math.cos(angle)
+const y = radius * Math.sin(angle)
+
+```
