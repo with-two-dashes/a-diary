1a65fe03f5990f73e95cbd736d0aee6f3338d4ec
Victor Lawrence <victor@victors-mbp.attlocal.net>
2019-11-09 12:31:03 -0500

    updated the coding math template a bit.

--- coding-math/template/main.js
+++ coding-math/template/main.js
@@ -4,14 +4,21 @@ import { clearCanvas } from '../common/clearCanvas.js'
 
 const canvas = document.createElement('canvas')
 document.body.appendChild(canvas)
+canvasSizer({ canvas })
 
 const context = canvas.getContext('2d')
 
 let shouldContinue = true
 
+const { width, height } = canvas
+
+// initialize Stuff here.
+
 const render = ({ timestamp, resized, context, canvas }) => {
   clearCanvas({ context })
   const { width, height } = canvas
+
+  // animate stuff here
 }
 
 const heartbeat = timestamp => {
