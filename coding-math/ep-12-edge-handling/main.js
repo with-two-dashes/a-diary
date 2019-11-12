8da63963c830c7ff94c7ada4aaa49e222cc84ac9
Victor Lawrence <victor@victors-mbp.attlocal.net>
2019-11-11 23:33:28 -0500

    Witness a removal.

--- coding-math/ep-12-edge-handling/main.js
+++ coding-math/ep-12-edge-handling/main.js
@@ -14,13 +14,18 @@ let shouldContinue = true
 
 const { width, height } = canvas
 
-const p = makeParticle({
-  x: width / 2,
-  y: height / 2,
-  speed: 4,
-  direction: Math.random() * Math.PI * 2,
-  radius: 50
-})
+const particleCount = 100
+let particlesArray = []
+
+for (let i = 0; i < particleCount; i++) {
+  particlesArray.push(makeParticle({
+    x: width / 2,
+    y: height / 2,
+    speed: 10 * Math.random() + 4,
+    direction: Math.random() * Math.PI * 2,
+    radius: Math.random() * 20,
+  }))
+}
 
 // initialize Stuff here.
 
@@ -28,24 +33,28 @@ const render = ({ timestamp, resized, context, canvas }) => {
   clearCanvas({ context })
   const { width, height } = canvas
 
-  p.update()
+  particlesArray = particlesArray.filter(p => {
 
-  context.beginPath()
-  context.arc(p.position.x, p.position.y, p.radius, 0, Math.PI * 2, false)
-  context.fill()
+    p.update()
 
-  if (p.position.x - p.radius > width) {
-    p.position.x = -p.radius
-  } else if (p.position.x < -p.radius) {
-    p.position.x = width + p.radius
-  }
+    context.beginPath()
+    context.arc(p.position.x, p.position.y, p.radius, 0, Math.PI * 2, false)
+    context.fill()
 
-  if (p.position.y - p.radius > height) {
-    p.position.y = -p.radius
-  } else if (p.position.y < -p.radius) {
-    p.position.y = height + p.radius
-  }
+    if (p.position.x - p.radius > width) {
+      return false
+    } else if (p.position.x < -p.radius) {
+      return false
+    }
 
+    if (p.position.y - p.radius > height) {
+      return false
+    } else if (p.position.y < -p.radius) {
+      return false
+    }
+
+    return true
+  })
 }
 
 const heartbeat = timestamp => {
--- coding-math/ep-12-edge-handling/readme.md
+++ coding-math/ep-12-edge-handling/readme.md
@@ -56,3 +56,47 @@ We don't always want things to live forever.
 
 Once we are done with something we can simply remove it.
 
+```js
+// ...
+
+const particleCount = 100
+let particlesArray = []
+
+for (let i = 0; i < particleCount; i++) {
+  particlesArray.push(makeParticle({
+    x: width / 2,
+    y: height / 2,
+    speed: 10 * Math.random() + 4,
+    direction: Math.random() * Math.PI * 2,
+    radius: Math.random() * 20,
+  }))
+}
+
+const render = ({ timestamp, resized, context, canvas }) => {
+  clearCanvas({ context })
+  const { width, height } = canvas
+
+  particlesArray = particlesArray.filter(p => {
+
+    p.update()
+
+    context.beginPath()
+    context.arc(p.position.x, p.position.y, p.radius, 0, Math.PI * 2, false)
+    context.fill()
+
+    if (p.position.x - p.radius > width) {
+      return false
+    } else if (p.position.x < -p.radius) {
+      return false
+    }
+
+    if (p.position.y - p.radius > height) {
+      return false
+    } else if (p.position.y < -p.radius) {
+      return false
+    }
+
+    return true
+  })
+}
+```
