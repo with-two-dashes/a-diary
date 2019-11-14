247a12552f3255b6c56f8aa47780fa17dd6510ca
Victor Lawrence <victor.lawrence@uga.edu>
2019-11-14 11:26:15 -0500

    some updates to the template. Now things are a little cleaner with a `world.js` file seperate from the `main.js` file

---
+++ coding-math/template/world.js
@@ -0,0 +1,24 @@
+import {
+  registerRenderCycle,
+  // getCanvas,
+  // getContext,
+  clearCanvas,
+  startRender
+} from './main.js'
+
+import { drawCircle } from '../common/drawCircle.js'
+// import { makeParticle } from '../common/makeParticle.js'
+// import { makeVector } from '../common/makeVector.js'
+
+// const { width, height } = getCanvas()
+
+registerRenderCycle(({ context, mousePosition }) => {
+  clearCanvas()
+  drawCircle({
+    context,
+    position: mousePosition,
+    radius: 10
+  })
+})
+
+startRender()
